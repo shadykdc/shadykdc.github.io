1504e62f637081ff3082d43121b192fa81c81ca1
Katie Shea <kashea@microsoft.com>
2017-02-13 11:00:39 -0800

    finished buttons and slideshow

--- home.js
+++ home.js
@@ -1,16 +1,60 @@
+/* kqshea.com javascript */
 
-var images = ["colorado_small.jpg",
-                "sunrise_small.jpg"];
-var captions = ["Colorado | August, 2015",
-                "Mt. Whiteny, CA | June, 2015"];
+/**
+ * Change the top image every 5 seconds
+ */
+var images = ["sunrise_small.jpg",
+              "colorado_small.jpg",
+              "dock.jpg"];
+var captions = ["Mt. Whiteny, CA | June, 2015",
+                "Colorado | August, 2015",
+                "Roche Harbor, WA | May, 2015"];
 var i = 0;
-var interval = setInterval(slideShow, 5000);
+var show = setInterval(nextSlideShowImage, 5000);
 
-function slideShow() {
+function refreshImage(){
+    if (i < 0) { i = images.length-1; }
     i = i%images.length;
     image = images[i];
     caption = captions[i];
     document.getElementById("slide").src=image;
     document.getElementById("slideCaption").innerHTML=caption;
-    i++;
+    clearInterval(show);
+    show = setInterval(nextSlideShowImage, 5000);
 }
+
+function nextSlideShowImage() {
+    i++;
+    refreshImage();
+}
+
+function prevSlideShowImage() {
+    i--;
+    refreshImage();
+}
+
+/**
+ * Insert buttons for images
+ */
+var nextButton = document.createElement("button");
+nextButton.className = "button-next";
+document.getElementById("show").appendChild(nextButton);
+nextButton.onclick = function() {
+    nextSlideShowImage();
+}
+
+var prevButton = document.createElement("button");
+prevButton.className = "button-prev";
+document.getElementById("show").appendChild(prevButton);
+prevButton.onclick = function() {
+    prevSlideShowImage();
+}
+
+var prevTriangle = document.createElement("div");
+prevTriangle.className = "triangle-prev";
+prevButton.appendChild(prevTriangle);
+
+
+var nextTriangle = document.createElement("div");
+nextTriangle.className = "triangle-next";
+nextButton.appendChild(nextTriangle);
